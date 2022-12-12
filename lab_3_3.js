4f9ed7482699a3520adc12c4f06d64e08726acaf
Olena <7890572@ukr.net>
2022-12-12 22:33:39 +0100

    Added check for maturity

--- lab_3_3.js
+++ lab_3_3.js
@@ -1,5 +1,8 @@
 const number =  Number(prompt('Введіть число'));
 
 if(number < 12){
-    console.log('Дитинство')
-}
+    console.log('Дитинство');
+} else if (number < 17){
+    console.log(Юність);
+} else {console.log ('Зрілість')}
+
